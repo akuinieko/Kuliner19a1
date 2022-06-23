bcbae89c3999e0ff0024026f6183c9f8bd0775bc
akuinieko <akuinieko@gmail.com>
2022-06-23 14:17:36 +0700

    assets

--- app/src/main/java/com/narasatya/kuliner19a1/MainActivity.java
+++ app/src/main/java/com/narasatya/kuliner19a1/MainActivity.java
@@ -1,30 +1,59 @@
 package com.narasatya.kuliner19a1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
-import android.content.Intent;
+
 import android.os.Bundle;
-import android.os.Handler;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static int SPLASH_TIME_OUT = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new Handler().postDelayed(new Runnable() {
+        BottomNavigationView bnvMenuBawah = findViewById(R.id.bnv_menu);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id. body_container, new HomeFragment()).commit();
+
+        bnvMenuBawah.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
-            public void run() {
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
-                Intent homeIntent = new Intent (MainActivity.this, HomeActivity.class);
-                startActivity(homeIntent);
-                finish();
+                Fragment FR = null;
 
+                switch (item.getItemId()){
+
+                    case R.id.menu_home:
+                        FR = new HomeFragment();
+                        getSupportActionBar();setTitle("Home");
+                        break;
+
+                    case R.id.menu_crud:
+                        FR = new CrudFragment();
+                        getSupportActionBar();setTitle("Crud");
+                        break;
+
+                    case R.id.menu_about:
+                        FR = new AboutFragment();
+                        getSupportActionBar();setTitle("About");
+                        break;
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id. body_container, FR).commit();
+
+                return true;
             }
-        }, SPLASH_TIME_OUT);
+        });
+
+
 
 
 
